4fe705a1cc12126aaf35af740815e49640e54b0f
Yelk11 <neighbour.matthew@gmail.com>
2021-11-02 11:08:20 -0400

    adding cuisine types

---
+++ epicurious_scraper.py
@@ -0,0 +1,35 @@
+
+cuisine = [
+    Italian,
+    Mexican,
+    Moroccan,
+    French,
+    Asian,
+    Indian,
+    Thai,
+    Mediterranean
+]
+
+
+
+
+
+
+'''
+Input:
+get_recipe_list(cuisine)
+cuisine:
+    Italian
+    Mexican
+    Moroccan
+    French
+    Asian
+    Indian
+    Thai
+    Mediterranean
+
+
+
+'''
+
+
