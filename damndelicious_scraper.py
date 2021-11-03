8ec4a9aa9b2ee64810c661a7253a96d7b87da155
parke1wh <whparker@oakland.edu>
2021-11-03 12:00:16 -0400

    scanner with json
    Was able to print the json well with the recipe for one exact URL

--- damndelicious_scraper.py
+++ damndelicious_scraper.py
@@ -1,1 +1,32 @@
+from recipe_scrapers import scrape_me
+import json
 
+# give the url as a string, it can be url from any site listed below
+scraper = scrape_me('https://steamykitchen.com/121682-mexican-street-corn-elote-in-the-microwave.html')
+
+# Q: What if the recipe site I want to extract information from is not listed below?
+# A: You can give it a try with the wild_mode option! If there is Schema/Recipe available it will work just fine.
+#scraper = scrape_me('https://www.bettycrocker.com/search?term=' + category, wild_mode=True)
+
+print(scraper.title())
+scraper.total_time()
+scraper.yields()
+scraper.ingredients()
+scraper.instructions()
+scraper.image()
+scraper.host()
+scraper.links()
+scraper.nutrients()  # if available
+
+recipe = {
+    "name":scraper.title(),
+    "time":str(scraper.total_time()) + ' minutes',
+    "yield":scraper.yields(),
+    "ingredients":scraper.ingredients(),
+    "instructions":scraper.instructions()
+}
+
+
+json_dump = json.dumps(recipe)
+
+print(json_dump)
