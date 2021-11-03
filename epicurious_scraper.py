b49c950cd485b4fc9eee707dbd7aa8ce5f8b1ea7
Yelk11 <neighbour.matthew@gmail.com>
2021-11-02 20:46:17 -0400

    Update epicurious_scraper.py

--- epicurious_scraper.py
+++ epicurious_scraper.py
@@ -29,17 +29,34 @@ x = {
 '''
 
 import json
-
 from recipe_scrapers import scrape_me
 import re
 
 
+# dont call this method!!
+def get_recipe(url):
+  scraper = scrape_me('https://www.epicurious.com'+ url)
+  
+  try:
+    print(scraper.title())
+  except:
+    print("no title found")
 
+  # print(scraper.title())
+  # print("hello")
+  # x = [
+  #   {
+  #     "Name": scraper.title(),
+  #     "Ingredients":scraper.ingredients()
+  #   }
+  # ]
+  #return x
 
+# Jared, use this method. It will return a JSON object that you pass to Nick
 def get_recipe_list(cuisine):
   search_url = "https://www.epicurious.com/search/?cuisine=" + cuisine + "&content=recipe"
   scraper = scrape_me(search_url)
-
+  
   #grab all links on search page
   link_list = []
   for line in scraper.links():
@@ -47,6 +64,30 @@ def get_recipe_list(cuisine):
       if re.search("/recipes/food/views/", link) and link not in link_list:
         link_list.append(link)
   
-  
+  recipe_list = []
+  for url in link_list:
+    recipe_list.append(get_recipe(url))
+  print(recipe_list)
 
+  #return my_json
+
+
+
+
+# Driver function to test
 get_recipe_list("italian")
+
+
+
+
+
+
+  
+scraper.total_time()
+scraper.yields()
+
+scraper.instructions()
+scraper.image()
+scraper.host()
+scraper.links()
+scraper.nutrients()  # if available
