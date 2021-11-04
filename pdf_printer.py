e0a06e0881156c09bac3e2bcf9844c035a19e61e
Kramenator <72082939+Kramenator@users.noreply.github.com>
2021-11-04 16:49:18 -0400

    template set up for pdf writer

--- pdf_printer.py
+++ pdf_printer.py
@@ -5,15 +5,25 @@
 import json
 from fpdf import FPDF
 
-title = "spaghetti"
-instructions = "r4fgfvefjwegfdwvfgjw"
-ingredients = ["er4e", "reref", "3ewrfe"]
+title = "Spaghetti"
+instructions = "Break noodles down to correct size. Add noodles to pot and pour water..."
+ingredients = ["Noodles(any kind)", "water", "cheese"]
+
 pdf = FPDF('P', 'mm', 'A4')
 
 pdf.add_page()
 
 pdf.set_font('times', '', 14)
 
-pdf.cell(40, 10, title)
+pdf.cell(40, 10, title, ln=True)
+
+pdf.cell(40, 10, instructions, ln=True)
+
+pdf.cell(40, 10, ingredients[0], ln=True)
+
+pdf.cell(40, 10, ingredients[1], ln=True)
+
+pdf.cell(40, 10, ingredients[2])
+
 
 pdf.output('recipes.pdf')
