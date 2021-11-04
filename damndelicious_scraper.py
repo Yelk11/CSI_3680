884ac86a5adf043611646dc69470f291b4f25891
parke1wh <whparker@oakland.edu>
2021-11-04 13:25:16 -0400

    Update damndelicious_scraper.py

--- damndelicious_scraper.py
+++ damndelicious_scraper.py
@@ -1,32 +1,26 @@
 from recipe_scrapers import scrape_me
-import json
+import json, re
 
-# give the url as a string, it can be url from any site listed below
-scraper = scrape_me('https://steamykitchen.com/121682-mexican-street-corn-elote-in-the-microwave.html')
+def get_recipe(url):
+  scraper = scrape_me('https://www.yummly.com'+ url)
+  print(scraper.title())
 
-# Q: What if the recipe site I want to extract information from is not listed below?
-# A: You can give it a try with the wild_mode option! If there is Schema/Recipe available it will work just fine.
-#scraper = scrape_me('https://www.bettycrocker.com/search?term=' + category, wild_mode=True)
-
-print(scraper.title())
-scraper.total_time()
-scraper.yields()
-scraper.ingredients()
-scraper.instructions()
-scraper.image()
-scraper.host()
-scraper.links()
-scraper.nutrients()  # if available
-
-recipe = {
-    "name":scraper.title(),
-    "time":str(scraper.total_time()) + ' minutes',
-    "yield":scraper.yields(),
-    "ingredients":scraper.ingredients(),
-    "instructions":scraper.instructions()
-}
+def steamyKitchen(category):
+  steamyKitchen = "https://www.yummly.com/recipes?q=" + category + "&taste-pref-appended=true"
+  scraper = scrape_me(steamyKitchen)
+  
+  #grab all links on search page
+  link_list = []
+  for line in scraper.links():
+      link = line.get('href')
+      if re.search("/recipe/", link) and link not in link_list:
+        link_list.append(link)
+  print(link_list)
+  
+  recipe_list = []
+  for url in link_list:
+    recipe_list.append(get_recipe(url))
+  print(recipe_list)
 
 
-json_dump = json.dumps(recipe)
-
-print(json_dump)
+steamyKitchen('mexican')
