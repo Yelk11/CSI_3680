5ea3256d61c6da134e44778bec3bf14b17b59952
Kramenator <72082939+Kramenator@users.noreply.github.com>
2021-11-06 14:13:32 -0400

    Converted Json to String and outputted it to PDF

--- pdf_printer.py
+++ pdf_printer.py
@@ -5,29 +5,17 @@
 import json
 from fpdf import FPDF
 
-title = "Spaghetti"
-instructions = "Break noodles down to correct size. Add noodles to pot and pour water..."
-ingredients = ["Noodles(any kind)", "water", "cheese"]
-
 pdf = FPDF('P', 'mm', 'A4')
 
 pdf.add_page()
 
 pdf.set_font('times', '', 14)
 
-pdf.cell(40, 10, title, ln=True)
-
-pdf.cell(40, 10, instructions, ln=True)
-
-pdf.cell(40, 10, ingredients[0], ln=True)
-
-pdf.cell(40, 10, ingredients[1], ln=True)
-
-pdf.cell(40, 10, ingredients[2], ln=True)
-
 with open('example.json') as f:
     recipes = json.load(f)
 
-print(recipes)
+recipesStr = json.dumps(recipes)
+
+pdf.multi_cell(0, 10, recipesStr)
 
 pdf.output('recipes.pdf')
