889234acb236f78768ce54baf9959a371bffba92
Kramenator <72082939+Kramenator@users.noreply.github.com>
2021-11-07 20:19:06 -0500

    added a new page for each recipe

--- pdf_printer.py
+++ pdf_printer.py
@@ -12,19 +12,19 @@ pdf.add_page()
 
 pdf.add_font("Roboto Slab", '', "RobotoSlab-Regular.ttf", uni=True)
 
-pdf.set_font('Roboto Slab', '', 12)
-
 matt = epic.epicurious_scraper("mexican")
 
 recipes = json.loads(matt)
 
 
 for recipe in recipes["recipe"]:
+    pdf.set_font('Roboto Slab', '', 12)
     pdf.cell(0, 10, recipe["name"], ln=True)
     for ingredient in recipe["ingredients"]:
         pdf.set_font('Roboto Slab', '', 10)
         pdf.cell(0, 10, ingredient, ln=True)
     #pdf.multi_cell(0, 10, recipe["directions"])
+    pdf.add_page()
     
 
 #METHOD THAT PRINTS JSON as String - Last Resort
