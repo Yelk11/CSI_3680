47b5081a226f630ca590e5c4e88b18ff1cee59bc
SamuelDillon0301 <73602175+SamuelDillon0301@users.noreply.github.com>
2021-11-24 12:16:52 -0500

    Add files via upload
    It works, but the formatting is weird.

--- json_to_html.py
+++ json_to_html.py
@@ -1,13 +1,15 @@
-#Output to HTML and PDF
+#Output to PDF
 
 #Project
 
 import json
 import PyPDF2
 from fpdf import FPDF
+from PyPDF2 import PdfFileReader
+from json2html import *
+import fitz
 
-def write_to_html(input_json):
-
+def write_to_pdf(input_json):
     pdf = FPDF('P', 'mm', 'A4')
 
     # setup pdf file
@@ -15,6 +17,7 @@ def write_to_html(input_json):
     pdf.add_font('Roboto', '', 'Roboto-Black.ttf', uni=True)
     pdf.set_font("Roboto", '', 12)
 
+    #Load recipe data from json and output to pdf format
     recipes = json.loads(input_json)
     for recipe in recipes["recipe"]:
         pdf.cell(0, 10, recipe['name'], ln=True)
@@ -24,34 +27,12 @@ def write_to_html(input_json):
         pdf.add_page()
     pdf.output('recipes.pdf')
 
-
-    pdfFileObj = open('recipes.pdf', 'rb')
-
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-
-    pageObj = pdfReader.getPage(0)
-
-    text = pageObj.extractText()
-
-    print(text)
-
-    html2="<p>" +text+ "</p>"
-    
-    pdfFileObj.close()
-
-    
-    
-    html="""
-    <html>
-        <body>
-            <h1>Recipes</h1>
-            <h2>Name goes here</h2>
-                <p> ingredients go here </p>
-                <p> instructions go here </p>
-        </body>
-    </html>
-    """
-
-    file = open('recipes.html','w')
-    file.write(html2)
-    file.close()
+    #Scrape PDF
+    with fitz.open("recipes.pdf") as doc:
+        text = ""
+        for page in doc:
+            text += page.getText()
+            html2=("<h1>Recipes</h1>"+("<p>"+text+"</p>"))
+            file = open('recipes.html','w')
+            file.write(html2)
+        file.close()
