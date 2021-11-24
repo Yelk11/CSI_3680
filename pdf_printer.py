f09945c149f856eae59b1d667d60539252916cc9
SamuelDillon0301 <73602175+SamuelDillon0301@users.noreply.github.com>
2021-11-24 12:56:09 -0500

    Add files via upload
    Use this instead of json_to_html.

--- pdf_printer.py
+++ pdf_printer.py
@@ -2,9 +2,12 @@
 
 #Project
 
-
 import json
+import PyPDF2
 from fpdf import FPDF
+from PyPDF2 import PdfFileReader
+from json2html import *
+import fitz
 
 def write_to_pdf(input_json):
     pdf = FPDF('P', 'mm', 'A4')
@@ -14,6 +17,7 @@ def write_to_pdf(input_json):
     pdf.add_font('Roboto', '', 'Roboto-Black.ttf', uni=True)
     pdf.set_font("Roboto", '', 12)
 
+    #Load recipe data from json and output to pdf format
     recipes = json.loads(input_json)
     for recipe in recipes["recipe"]:
         pdf.cell(0, 10, recipe['name'], ln=True)
@@ -23,4 +27,12 @@ def write_to_pdf(input_json):
         pdf.add_page()
     pdf.output('recipes.pdf')
 
-
+    #Scrape PDF
+    with fitz.open("recipes.pdf") as doc:
+        text = ""
+        for page in doc:
+            text += page.getText()
+            html2=("<h1>Recipes</h1>"+("<p>"+text+"<br>"+"</p>"))
+            file = open('recipes.html','w')
+            file.write(html2)
+        file.close()
